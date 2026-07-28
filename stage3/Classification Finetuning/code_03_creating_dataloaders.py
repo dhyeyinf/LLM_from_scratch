cd009580a3269f2fea0c9f2d1c74537ccfeea6b0
Dhyey Findoriya <131277481+dhyeyinf@users.noreply.github.com>
2026-07-28 15:00:08 +0530

    Create code_03_creating_dataloaders.py

---
+++ stage3/Classification Finetuning/code_03_creating_dataloaders.py
@@ -0,0 +1,28 @@
+from torch.utils.data import DataLoader
+
+num_workers = 0
+batch_size = 8
+
+torch.manual_seed(123)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True,
+)
+
+val_loader = DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
